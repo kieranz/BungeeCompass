0aa97daeb19ca2380c586c8424e130860d2786e2
kieranz <kieranckeith@gmail.com>
2019-08-07 14:12:29 -1000

    Update and rename Comando.java to Command.java

--- src/main/java/me/netindev/comandos/Command.java
+++ src/main/java/me/netindev/comandos/Command.java
@@ -9,7 +9,7 @@
  *  org.bukkit.plugin.Plugin
  *  org.bukkit.plugin.PluginDescriptionFile
  */
-package me.netindev.comandos;
+package me.netindev.command;
 
 import me.netindev.Main;
 import org.bukkit.command.Command;
@@ -19,7 +19,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 
-public class Comando
+public class Command
 implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
@@ -35,13 +35,13 @@ implements CommandExecutor {
                 }
             }
         } else {
-            Player jogador = (Player)sender;
+            Player player = (Player) sender;
             if (jogador.hasPermission("bungeecompass.command")) {
                 if (args.length == 0 || args.length > 1) {
-                    jogador.sendMessage("\u00a7f[\u00a7aBungeeCompass\u00a7f] \u00a7aV: \u00a7f" + Main.plugin.getDescription().getVersion() + "\u00a7a.");
-                    jogador.sendMessage("\u00a7aPlugin created by: \u00a7fnetindev\u00a7a.");
-                    jogador.sendMessage("\u00a7aCommands:");
-                    jogador.sendMessage("\u00a7f/bungeecompass reload / \u00a7aReload this plugin\u00a7f.");
+                    player.sendMessage("\u00a7f[\u00a7aBungeeCompass\u00a7f] \u00a7aV: \u00a7f" + Main.plugin.getDescription().getVersion() + "\u00a7a.");
+                    player.sendMessage("\u00a7aPlugin created by: \u00a7fnetindev\u00a7a.");
+                    player.sendMessage("\u00a7aCommands:");
+                    player.sendMessage("\u00a7f/bungeecompass reload / \u00a7aReload this plugin\u00a7f.");
                 } else if (args.length == 1) {
                     if (args[0].equalsIgnoreCase("reload")) {
                         Main.plugin.reloadConfig();
